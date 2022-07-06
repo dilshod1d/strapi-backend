ffc32e37d1bfa0c1e8bd652f08b964ec6d2e0ad2
Dilshod Shoolimkhonov <dilshodshoolimkhonov@MacBook-Pro-Dilshod.local>
2022-07-06 16:51:53 +0500

    order implemented

--- src/api/order/controllers/order.js
+++ src/api/order/controllers/order.js
@@ -1,4 +1,5 @@
 "use strict";
+const { sanitizeEntity } = require("strapi-utils");
 
 /**
  *  order controller
@@ -6,22 +7,48 @@
 
 const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController("api::order.order", ({ strapi }) => ({
-  async create(ctx) {
-    const user = ctx.state.user ? ctx.state.user.email : null;
-    const { name, phone, product, quantity, total, image } = ctx.request.body;
+module.exports = createCoreController("api::order.order");
 
-    const order = await strapi.db.query("api::order.order").create({
-      data: {
-        name,
-        phone,
-        product,
-        image,
-        quantity,
-        total,
-        user,
-      },
-    });
-    return order;
-  },
-}));
+// ({ strapi }) => ({
+//   async create(ctx) {
+//     const user = ctx.state.user ? ctx.state.user.id : null;
+//     const { name, phone, products, details } = ctx.request.body;
+//     const order = await strapi.db.query("api::order.order").create({
+//       data: {
+//         name,
+//         phone,
+//         products,
+//         details,
+//         user,
+//       },
+//     });
+//     return order;
+//   },
+//   async find(ctx) {
+//     const { user } = ctx.state;
+//     let entities;
+//     if (ctx.query._q) {
+//       entities = await strapi.services.order.search({
+//         ...ctx.query,
+//         user: user.id,
+//       });
+//     } else {
+//       entities = await strapi.services.order.find({
+//         ...ctx.query,
+//         user: user.id,
+//       });
+//     }
+//     return entities.map((entity) =>
+//       sanitizeEntity(entity, { model: strapi.models.order })
+//     );
+//   },
+//   async findOne(ctx) {
+//     const { id } = ctx.params;
+//     const { user } = ctx.state;
+//     const entity = await strapi.services.order.findOne({
+//       id,
+//       user: user.id,
+//     });
+//     return sanitizeEntity(entity, { model: strapi.models.order });
+//   },
+// })
